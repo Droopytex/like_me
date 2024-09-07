8560ea374a2b06f810ccb31943e8114e2c0e3f08
Droopytex <a.velasquezvega@gmail.com>
2024-09-06 23:35:05 -0400

    configuracion de funciones.js y index.js. Opercaiones GET y POST test

---
+++ like_me/backend/index.js
@@ -0,0 +1,24 @@
+const { leerPosts, escribirPosts } = require("./funciones");
+const cors = require("cors");
+
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log("Servidor escuhando en puerto 3000!"));
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/posts", async (req, res) => {
+  const obtenerPost = await leerPosts();
+  res.json(obtenerPost);
+});
+
+app.post("/posts", async (req, res) => {
+  const { titulo, url, descripcion } = req.body;
+  await escribirPosts(titulo, url, descripcion);
+  res.send("El post fue agregado");
+});
